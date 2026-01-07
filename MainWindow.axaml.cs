ec610b09d5d9f1305d2950ed5ee7b71b335e74da
Nyx <dudds222@gmail.com>
2026-01-07 13:09:41 -0300

    feat: implementando salvamento e carregamento via JSON

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -3,6 +3,8 @@ using Avalonia.Interactivity;
 using Avalonia.Input;
 using System.Collections.Generic;
 using Argos.Models;
+using System.IO;
+using System.Text.Json;
 
 
 namespace Argos;
@@ -11,10 +13,12 @@ public partial class MainWindow : Window
 {
     // Memória RAM do programa
     private List<Tarefa> _minhasTarefas = new List<Tarefa>();
+    private const string ArquivoJson = "tarefas.json";
 
     public MainWindow()
     {
         InitializeComponent();
+        CarregarDoArquivo();
     }
 
     // Função central 
@@ -45,6 +49,8 @@ public partial class MainWindow : Window
 
         //4. Limpar a caixa para a próxima
         CaixaDeTexto.Text = "";
+
+        SalvarNoArquivo();
     }
 
     // Mouse/Clique
@@ -59,6 +65,39 @@ public partial class MainWindow : Window
         if (args.Key == Key.Enter)
         {
             AdicionarNovaTarefa();
+        }
+    }
+    // Transforma memória em arquivo
+    private void SalvarNoArquivo()
+    {
+        // Serializar a lista de tarefas para JSON
+        string json = JsonSerializer.Serialize(_minhasTarefas, new JsonSerializerOptions { WriteIndented = true});
+      
+        // Escreve texto no disco rígido
+        File.WriteAllText(ArquivoJson, json);
+    }
+
+        // Arquivo em memória
+        private void CarregarDoArquivo()
+    {
+        // Verifica se o arquivo existe
+        if (!File.Exists(ArquivoJson)) return;
+        {
+            // Lê o conteúdo do arquivo
+            string json = File.ReadAllText(ArquivoJson);
+            
+            // Desserializa o JSON para a lista de tarefas
+            _minhasTarefas = JsonSerializer.Deserialize<List<Tarefa>>(json) ?? new List<Tarefa>();
+            // O "?? new List<Tarefa>()" é segurança em caso de arquivo corrompido/vazio.
+
+            // Atualiza a interface com as tarefas carregadas
+            foreach (var tarefa in _minhasTarefas)
+            {
+                CheckBox checkboxVisual = new CheckBox();
+                checkboxVisual.Content = tarefa.Descricao;
+                checkboxVisual.IsChecked = tarefa.Concluida;
+                ListaDeTarefas.Children.Add(checkboxVisual);
+            }
         }
     }
 }
